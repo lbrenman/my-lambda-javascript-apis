0c82f45ec5df628e06cf5f1fdd09e165140bcc4e
Leor Brenman <lbrenman99@hotmail.com>
2024-07-16 14:39:51 -0400

    update image functions

--- ImageUploader/DeleteImage/index.mjs
+++ ImageUploader/DeleteImage/index.mjs
@@ -1,5 +1,5 @@
 import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
-const s3Client = new S3Client({region: 'us-east-1'});
+const s3Client = new S3Client({region: process.env.REGION});
 
 export const handler = async (event) => {
     
--- ImageUploader/PostImage/index.mjs
+++ ImageUploader/PostImage/index.mjs
@@ -2,19 +2,19 @@ import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3
 
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3Client = new S3Client({region:'us-east-1'});
+const s3Client = new S3Client({region: process.env.REGION});
 
 export const handler = async (event) => {
     
-    console.log(event.body);
+    //console.log(event.body);
     const body = JSON.parse(event.body);
     
     const bucketName = process.env.BUCKETNAME;
-    const fileName = body.filename;
+    const fileName = `${body.filename}-${Date.now()}.jpg`;
     const base64String = body.image;
 
-    console.log(fileName);
-    console.log(base64String);
+    //console.log(fileName);
+    //console.log(base64String);
 
     // Decode the base64 string
     const buffer = Buffer.from(base64String, 'base64');
@@ -25,8 +25,8 @@ export const handler = async (event) => {
             Bucket: bucketName,
             Key: fileName,
             Body: buffer,
-            ContentEncoding: 'base64', // required if you are handling base64 encoding/decoding
-            ContentType: 'image/jpeg' // adjust accordingly if it's a different image type
+            ContentEncoding: 'base64',
+            ContentType: 'image/jpeg'
         });
         await s3Client.send(putObjectCommand);
         
@@ -37,9 +37,8 @@ export const handler = async (event) => {
         });
         
         // Generate a pre-signed URL - NOT WORKING - GENERATING ERROR: A region must be set when sending requests to S3
-        // const signedUrl = await getSignedUrl(s3Client, GetObjectCommand, { expiresIn: 3600 }); // URL expires in 1 hour
+        //const signedUrl = await getSignedUrl(s3Client, GetObjectCommand, { expiresIn: 3600 }); // URL expires in 1 hour
 
-        
         // Construct the URL of the uploaded file
         const fileUrl = `https://${bucketName}.s3.amazonaws.com/${fileName}`;
 
